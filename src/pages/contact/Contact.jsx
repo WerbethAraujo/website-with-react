228e69ca7b3d6ea7d78bcbdedec9de0b812a52ec
Werbeth Araujo <werbeth1991pbs@gmail.com>
2023-03-05 20:30:31 -0300

    feat: contact page done

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,48 @@
+import Header from '../../components/Header';
+import headerImage from '../../images/header_bg_2.jpg';
+import { MdEmail } from 'react-icons/md';
+import { BsMessenger } from 'react-icons/bs';
+import { IoLogoWhatsapp } from 'react-icons/io';
+
 import './contact.css';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <Header image={headerImage} title='Get In Touch'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+        consequuntur corporis nemo, vel consectetur hic adipisci ducimus.
+      </Header>
+
+      <section className='contact'>
+        <div className='container contact__container'>
+          <div className='contact__wrapper'>
+            <a
+              href='support@gmail.com'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              <MdEmail />
+            </a>
+            <a
+              href='support@gmail.com'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              <BsMessenger />
+            </a>
+            <a
+              href='support@gmail.com'
+              target='_blank'
+              rel='noreferrer noopener'
+            >
+              <IoLogoWhatsapp />
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Contact;
