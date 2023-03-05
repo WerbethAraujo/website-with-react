a6ff0d11c53f2d03c06b08e4f0dd510377f4b20b
Werbeth Araujo <werbeth1991pbs@gmail.com>
2023-03-04 23:36:15 -0300

    feat: section faqs done

---
+++ src/components/FAQs.jsx
@@ -0,0 +1,23 @@
+import { FaQuestion } from 'react-icons/fa';
+
+import SectionHead from './SectionHead';
+import FAQ from './FAQ';
+
+import { faqs } from '../data';
+
+const FAQs = () => {
+  return (
+    <section className='faqs'>
+      <div className='container faqs__container'>
+        <SectionHead icon={<FaQuestion />} title='FAQs' />
+        <div className='faqs__wrapper'>
+          {faqs.map(({ id, question, answer }) => (
+            <FAQ key={id} question={question} answer={answer} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default FAQs;
