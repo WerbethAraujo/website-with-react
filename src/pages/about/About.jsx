b95fe04acd23f2c459ae507522240531ec68d8d4
Werbeth Araujo <werbeth1991pbs@gmail.com>
2023-03-05 17:10:27 -0300

    feat:create header component

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,15 @@
+import Header from '../../components/Header';
+
+import bgImage01 from '../../images/header_bg_1.jpg';
 import './about.css';
 
 const About = () => {
-  return <div>Abaout</div>;
+  return (
+    <Header image={bgImage01} title='About Us'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, a. Ab
+      possimus fugit doloremque sed quam dolor explicabo consequuntur tenetur.
+    </Header>
+  );
 };
 
 export default About;
