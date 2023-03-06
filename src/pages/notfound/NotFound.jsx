a9058482b06fab84678a0125d7a20dd94452077a
Werbeth Araujo <werbeth1991pbs@gmail.com>
2023-03-05 22:00:22 -0300

    feat: not found page done

--- src/pages/notfound/NotFound.jsx
+++ src/pages/notfound/NotFound.jsx
@@ -1,7 +1,18 @@
-import './not_found.css';
+import { Link } from 'react-router-dom';
+
+import './notFound.css';
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <section className='notfound'>
+      <div className='container notfound__container'>
+        <h2>Page Not Found</h2>
+        <Link to='/' className='btn'>
+          Go Back Home
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default NotFound;
