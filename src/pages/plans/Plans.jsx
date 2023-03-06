79deed4c6c112d5b1c4bcb90f0917bb31f2dbf68
Werbeth Araujo <werbeth1991pbs@gmail.com>
2023-03-05 22:42:13 -0300

    feat: plans page done

--- src/pages/plans/Plans.jsx
+++ src/pages/plans/Plans.jsx
@@ -1,7 +1,40 @@
+import Header from '../../components/Header';
+import headerImage from '../../images/header_bg_4.jpg';
+
+import Card from '../../UI/Card';
+
+import { plans } from '../../data';
+
 import './plans.css';
 
 const Plans = () => {
-  return <div>Plans</div>;
+  return (
+    <>
+      <Header image={headerImage} title='Membership Plans'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis in,
+        adipisci accusamus, iure repellat autem magni? eaque?
+      </Header>
+      <section className='plans'>
+        <div className='container plans__container'>
+          {plans.map(({ id, name, desc, price, features }) => (
+            <Card key={id} className='plan'>
+              <h3>{name}</h3>
+              <small>{desc}</small>
+              <h1>{`R$${price}`}</h1>
+              <h2>/mês</h2>
+              <h4>Features</h4>
+              {features.map(({ feature, available }, index) => (
+                <p key={index} className={!available ? 'disabled' : ''}>
+                  {feature}
+                </p>
+              ))}
+              <button className='btn btn-lg'>Choose Plan</button>
+            </Card>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Plans;
