42b4ad1e6334652790a5488eafef82bcffeeeaa6
Werbeth Araujo <werbeth1991pbs@gmail.com>
2023-03-05 21:00:43 -0300

    feat: gallery page done

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,7 +1,33 @@
+import Header from '../../components/Header';
+import headerImage from '../../images/header_bg_3.jpg';
+
 import './gallery.css';
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const imagesLenght = 15;
+  const images = [];
+
+  for (let i = 1; i <= imagesLenght; i++) {
+    images.push(require(`../../images/gallery${i}.jpg`));
+  }
+  return (
+    <>
+      <Header image={headerImage} title='Our Gallery'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque sunt
+        quia earum voluptatem non rem? Iure corrupti repellendus ratione id.
+      </Header>
+
+      <section className='gallery'>
+        <div className='container gallery__container'>
+          {images.map((image, index) => (
+            <article key={index}>
+              <img src={image} alt={`gallery images ${index + 1}`} />
+            </article>
+          ))}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Gallery;
